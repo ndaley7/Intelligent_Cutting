a370c69942c20c4ec6b465daf48d849b04552850
Unknown <ndaley7@gatech.edu>
2018-03-04 20:23:23 -0500

    File Rearrangement
    Shuffled the PFH files

---
+++ Descriptors/PFH/PFH_K500_N3mm_D5mm/PlotScript.m
@@ -0,0 +1,101 @@
+%Quick plot script
+% figure
+% stem(SingleKeelFH,'r')
+% title('Front Keel')
+% axis([-inf inf 0 40])
+% figure
+% stem(SingleKeelWog,'g')
+% title('Wog Keel')
+% axis([-inf inf 0 40])
+% figure
+% stem(SingleLegLWog,'b')
+% title('Wog Leg L')
+% axis([-inf inf 0 40])
+% figure
+% stem(SingleLegRWog,'y')
+% title('Wog Leg R')
+% axis([-inf inf 0 40])
+% figure
+% stem(SingleP21Wog,'m')
+% title('Wog P21')
+% axis([-inf inf 0 40])
+% %P22
+% figure
+% hold
+% stem(P22224PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% %figure
+% stem(P22225PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% %figure
+% stem(P22226PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% %figure
+% stem(P22227PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% %figure
+% stem(P22233PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% %figure
+% stem(P22234PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% %figure
+% stem(P22235PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% figure
+% stem(P22236PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% figure
+% stem(P22241PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+% figure
+% stem(P22242PFH0(1,:),'c')
+% title('Wog P22')
+% axis([-inf inf 0 40])
+%Other
+figure
+hold
+stem(Other001PFH0(1,:),'c')
+title('Other')
+axis([-inf inf 0 40])
+%figure
+stem(Other001PFH2(1,:),'c')
+title('Other')
+axis([-inf inf 0 40])
+%figure
+stem(Other001PFH3(1,:),'c')
+title('Other')
+axis([-inf inf 0 40])
+%figure
+stem(Other001PFH5(1,:),'c')
+title('Other')
+axis([-inf inf 0 40])
+%figure
+stem(Other001PFH6(1,:),'c')
+title('Other')
+axis([-inf inf 0 40])
+%figure
+stem(Other001PFH7(1,:),'c')
+title('Other')
+axis([-inf inf 0 40])
+%figure
+stem(Other001PFH8(1,:),'c')
+title('Other')
+axis([-inf inf 0 40])
+% figure
+% stem(SingleP23Wog,'b')
+% title('Wog P23')
+% axis([-inf inf 0 40])
+% figure
+% stem(SingleP22FH,'k')
+% title('Front P22')
+% axis([-inf inf 0 40])
